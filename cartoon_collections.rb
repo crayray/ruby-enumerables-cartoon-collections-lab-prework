57e304cafb8fea65714c064dbeeef3ef04ed1706
Rachel Fuerst <rach.fuerst@gmail.com>
2019-10-23 21:47:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.find{|cheese| cheese == "cheddar" || "gouda" || "camambert" }
-    return array.find{|cheese| cheese == "cheddar" || "gouda" || "camambert" }
+    return array.find{|cheese|  cheese_types.include?(cheese) }
   else
     return nil
   end
