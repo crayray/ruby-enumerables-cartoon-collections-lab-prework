5091cf39d67835c3a113a0f8afdce1c005aab0d7
Rachel Fuerst <rach.fuerst@gmail.com>
2019-10-23 21:20:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)# code an argument here
-  array.map{|elem| puts "elem.capitalize !"}
+  array.map{|elem| elem.capitalize "!"}
 end
 
 def long_planeteer_calls# code an argument here
