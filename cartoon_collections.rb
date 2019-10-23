ebf989d81e0c18c4eb15fc101110db080fbee485
Rachel Fuerst <rach.fuerst@gmail.com>
2019-10-23 21:34:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def summon_captain_planet(array)# code an argument here
 end
 
 def long_planeteer_calls(array)
-  char = 0
+  # char = 0
   if array.any?{|char| char.length > 4}
     return true
   else
